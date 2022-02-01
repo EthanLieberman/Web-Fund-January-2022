0f1fac15e8b81e8e010587b387c2e1b43ea0fc7a
Ethan Lieberman <ethan.lieberman@yahoo.com>
2022-02-01 15:28:28 -0800

    update

--- Lieberman_Ethan_Dojo_Weather/script.js
+++ Lieberman_Ethan_Dojo_Weather/script.js
@@ -1,9 +1,25 @@
-
-
-
-
-function clik(){
+function clik() {
     var element = document.getElementById("vanish");
 
     element.remove();
+}
+
+
+function tempchange(select){
+
+    var el = document.querySelectorAll('span')
+console.log(el)
+    if (select.value == 'cel'){
+        for(var i = 0; i < el.length; i++){
+            el[i].innerText = Math.round((el[i].innerText - 32) * (5/9))
+
+        }
+
+    }
+
+    else{
+        for(var i = 0; i < el.length; i++){
+            el[i].innerText = Math.round((el[i].innerText * (9/5)) + 32)
+        }
+    }
 }
